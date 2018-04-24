473784685a9412460c5b0123b8877e4944583d94
liranfar <l.farage@gmail.com>
2018-04-24 15:30:31 +0300

    task 1  - complete winning logic

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,41 @@
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
+  const isWin = symbol => {
+    // rows
+    const isRowWin =
+      board[0].every(cell => cell === symbol) ||
+      board[1].every(cell => cell === symbol) ||
+      board[2].every(cell => cell === symbol);
+    // cols
+    const isColWin =
+      (board[0][0] === symbol && board[1][0] === symbol && board[2][0] === symbol) ||
+      (board[0][1] === symbol && board[1][1] === symbol && board[2][1] === symbol) ||
+      (board[0][2] === symbol && board[1][2] === symbol && board[2][2] === symbol);
+
+    const isDiagonalWin =
+      (board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol) ||
+      (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol);
+
+    return isRowWin || isColWin || isDiagonalWin;
+  };
+
+  const isBoardCovered = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] !== '') {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
   if (isWin('X')) {
     return 'X';
   }
   if (isWin('O')) {
     return 'O';
+  }
+  if (isBoardCovered) {
+    return '-';
   }
 };
--- src/gameService.unit.js
+++ src/gameService.unit.js
@@ -1,6 +1,46 @@
 const { gameStatus } = require('./gameService');
 
-test('X should win', () => {
+test('X should win first row', () => {
   const board = [['X', 'X', 'X'], ['', '', ''], ['', '', '']];
   expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win second row', () => {
+  const board = [['', '', ''], ['X', 'X', 'X'], ['', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win third row', () => {
+  const board = [['', '', ''], ['', '', ''], ['X', 'X', 'X']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win first col', () => {
+  const board = [['X', '', ''], ['X', '', ''], ['X', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win second col', () => {
+  const board = [['', 'X', ''], ['', 'X', ''], ['', 'X', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win third col', () => {
+  const board = [['', '', 'X'], ['', '', 'X'], ['', '', 'X']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win left diagonal', () => {
+  const board = [['X', '', ''], ['', 'X', ''], ['', '', 'X']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win right diagonal', () => {
+  const board = [['', '', 'X'], ['', 'X', ''], ['X', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('should be tie status', () => {
+  const board = [['X', 'O', 'O'], ['O', 'X', 'X'], ['X', 'O', 'O']];
+  expect(gameStatus(board)).toBe('-');
 });
